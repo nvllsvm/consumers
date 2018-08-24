89cdb356bde92536c4e6ba6aa000f89a752fb1b7
Andrew Rabert <ar@nullsum.net>
2018-08-24 12:07:35 -0400

    Use list for setup classifiers

--- setup.py
+++ setup.py
@@ -19,12 +19,12 @@ setuptools.setup(
     url='https://github.com/nvllsvm/consumers',
     license='Apache 2.0',
     packages=['consumers'],
-    classifiers=(
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3 :: Only'
-    ),
+    ],
     setup_requires=setup_requires,
     tests_require=['pytest', 'pytest-cov'],
     zip_safe=True
